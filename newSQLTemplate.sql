c3469626d7e79e8fda6d659f233fc315c49cb6cd
PujaAmmineni <pamminen@mtu.edu>
2024-09-11 09:21:26 -0400

    Initial commit of Apache project

---
+++ newSQLTemplate.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Puja
+ * Created: 11 Sept 2024
+ */
+
---
+++ src/java/sun/misc/BASE64Encoder.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sun.misc;
+
+/**
+ *
+ * @author Puja
+ */
+public class BASE64Encoder {
+
+    public String encode(byte[] byteCipherText) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
